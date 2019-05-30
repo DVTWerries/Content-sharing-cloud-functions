c2509393148e5336954dc1caca7c787974862124
Len Minders <>
2019-05-30 12:36:35 +0200

    rename variables, use user uid instead of email

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,18 +6,16 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
-export const helloWorld = functions.storage.bucket().object().onFinalize(
-    response => {
-        const url = `https://firebasestorage.googleapis.com/v0/b/${response.bucket}/o/${response.name!.replace('/', '%2F')}?alt=media&token=${response.metadata!.firebaseStorageDownloadTokens}`;
-        const user = response.id.split('/');
-        admin.database().ref(user[1].replace(/\./g, '_')).push({
-            id: user[3],
-            imageUrl: url
-        });
-    }
-);
+export const storeMediaInfo = functions.storage.bucket().object().onFinalize((response) => {
+    const url = `https://firebasestorage.googleapis.com/v0/b/${response.bucket}/o/${response.name!.replace('/', '%2F')}?alt=media&token=${response.metadata!.firebaseStorageDownloadTokens}`;
+    const path = response.id.split('/');
+    admin.database().ref(path[1]).push({
+        id: path[3],
+        imageUrl: url
+    });
+});
 
-export const storeUser = functions.auth.user().onCreate((user) => {
+export const storeUserInfo = functions.auth.user().onCreate((user) => {
     // tslint:disable-next-line: no-floating-promises
     admin.database().ref(user.uid).set({
         email: user.email,
