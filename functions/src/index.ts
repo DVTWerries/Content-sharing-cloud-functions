079d45a9fdaded67c87210e8cb1cd0786ac4ecd9
Len Minders <>
2019-06-18 08:56:46 +0200

    add more info to image details

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,12 +9,16 @@ admin.initializeApp();
 export const storeMediaInfo = functions.storage.bucket().object().onFinalize((response) => {
     const url = `https://firebasestorage.googleapis.com/v0/b/${response.bucket}/o/${response.name!.replace('\/', '%2F')}?alt=media&token=${response.metadata!.firebaseStorageDownloadTokens}`;
     const path = response.id.split('/');
+
     // tslint:disable-next-line: no-floating-promises
     admin.database().ref(`${path[1]}/files/${path[2].split('.')[0]}`).update({
-            id: path[2].split('.')[0],
-            imageName: path[2],
-            imageUrl: url
-        });
+        id: path[2].split('.')[0],
+        imageName: path[2],
+        imageUrl: url,
+        fileSize: response.size,
+        fileExtention: path[2].split('.').pop(),
+        contentType: response.contentType,
+    });
 });
 
 export const deleteMediaInfo = functions.storage.bucket().object().onDelete((response) => {
